8ea35765f6c0a5b4490a2daebd9be23e08287087
Ozocak Ibrahim <o22204518@or-iut-i202-f17.iut45.univ-orleans.fr>
2025-01-17 13:01:14 +0100

    multi a jour

--- Multimedia/canevasTD5/td5.cpp
+++ Multimedia/canevasTD5/td5.cpp
@@ -8,7 +8,7 @@
 #define GL_SILENCE_DEPRECATION
 #include <OpenGL/gl3.h>
 #include <GLUT/glut.h>
- 
+
 #else
 #include <GL/glew.h>
 #include <GL/glut.h>
@@ -26,6 +26,8 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include <sys/timeb.h>
+
 repere rep(1.0);
 
 unsigned int progid;
@@ -33,9 +35,6 @@ unsigned int mvpid;
 unsigned int mid;
 unsigned int vid;
 unsigned int pid;
-
-
-
 
 // Matrices 4x4 contenant les transformations.
 glm::mat4 model;
@@ -46,42 +45,130 @@ float angle = 0.0f;
 float scale = 0.0f;
 float inc = 0.1f;
 
-unsigned int vaoids[ 1 ];
+unsigned int vaoids[1];
 
 unsigned int nbtriangles;
 
 float x, y, z;
 
-std::array< float, 3 > eye = { 0.0f, 0.0f, 5.0f };
+std::array<float, 3> eye = {0.0f, 0.0f, 5.0f};
 
-    #define NBMESHES 4
+#define NBMESHES 4
 
-    struct shaderProg
-    {
+struct shaderProg
+{
     unsigned int progid; // ID du shader
-    unsigned int mid; // ID de la matrice de modelisation qui est passÃ©e en variable uniform au shader de vertex
+    unsigned int mid;    // ID de la matrice de modelisation qui est passÃ©e en variable uniform au shader de vertex
     unsigned int vid;
     unsigned int pid;
     unsigned int LightID;
-    }shaders[NBMESHES];
+} shaders[NBMESHES];
 
-
-    struct maillage
-    {
+struct maillage
+{
     shaderProg shader;
-    unsigned int vaoids; // VaoID qui contient les VBO oÃ¹ ce trouve ce maillage (et ses normales etc..) dans la VRAM
+    unsigned int vaoids;
     unsigned int nbtriangles;
     float angle = 0.0f;
-    float scale = 0.0f; // stocke l'opÃ©ration de normalisation de la taille du maillage (rÃ©alisÃ© lors de la lecture du maillage)
+    float scale = 0.0f;
     float inc = 0.1f;
-    float x, y, z; // stocke la position du centre du  maillage (rÃ©alisÃ© lors de la lecture du maillage) il faut utiliser cette info lors du dessin afin de toujours veiller Ã  recentrer le maillage en (0,0,0)
-
-    };
+    float x, y, z;
+};
 
 maillage maillages[NBMESHES];
 
-float rotationX = 0.0f;
-float rotationY = 0.0f;
+float mouvementX = 0.0f;
+float mouvementZ = 0.0f;
+
+float sensitivity = 0.2f; // Sensibilité souris
+float yaw = -90.0f;
+float pitch = 0.0f;
+bool firstMouse = true;
+
+glm ::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 5.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+glm::vec3 cameraRight = glm::vec3(1.0f, 0.0f, 0.0f);
+
+void updateCameraMouse()
+{
+    // Recalcul des vecteurs avant et haut de la caméra à partir de l'angle de yaw et pitch
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
+
+    // Calcul des vecteurs Right et Up
+    cameraRight = glm::normalize(glm::cross(cameraFront, glm::vec3(0.0f, 1.0f, 0.0f)));
+    cameraUp = glm::normalize(glm::cross(cameraRight, cameraFront));
+}
+
+float lastX = 320.0f; // Position initiale de la souris (au centre de l'écran)
+float lastY = 240.0f;
+
+void deplacementSouris(int xpos, int ypos)
+{
+    static bool firstMouse = true;
+    static float lastX = 400, lastY = 300;
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    updateCameraMouse();
+}
+
+int getMilliCount()
+{
+    timeb tb;
+    ftime(&tb);
+    int nCount = tb.millitm + (tb.time & 0xfffff) * 1000;
+    return nCount;
+}
+
+int getMilliSpan(int nTimeStart)
+{
+    int nSpan = getMilliCount() - nTimeStart;
+    if (nSpan < 0)
+        nSpan += 0x100000 * 1000;
+    return nSpan;
+}
+
+int lastTime = 0;
+double elapsed = double(getMilliSpan(lastTime)) / 1000.0;
+
+void calcTime()
+{
+
+    if (lastTime != 0)
+    {
+
+        elapsed = double(getMilliSpan(lastTime)) / 1000.0;
+        eye[0] += mouvementX * elapsed;
+        eye[2] += mouvementZ * elapsed;
+    }
+    lastTime = getMilliCount();
+}
 
 void displayMesh(maillage mesh, glm::mat4 model)
 {
@@ -94,7 +181,7 @@ void displayMesh(maillage mesh, glm::mat4 model)
     glUniformMatrix4fv(mesh.shader.mid, 1, GL_FALSE, &model[0][0]);
     glUniformMatrix4fv(mesh.shader.vid, 1, GL_FALSE, &view[0][0]);
     glUniformMatrix4fv(mesh.shader.pid, 1, GL_FALSE, &proj[0][0]);
-    
+
     glBindVertexArray(mesh.vaoids);
     glDrawElements(GL_TRIANGLES, mesh.nbtriangles * 3, GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
@@ -102,94 +189,105 @@ void displayMesh(maillage mesh, glm::mat4 model)
 
 void display()
 {
-    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-    view = glm::lookAt( glm::vec3( eye[ 0 ], eye[ 1 ], eye[ 2 ] ),
-      glm::vec3( eye[ 0 ], eye[ 1 ], eye[ 2 ]-1.0f ),
-      glm::vec3( 0.0f, 1.0f, 0.0f ) );
- 
+    calcTime();
 
-    float decal=1.25f;
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // view = glm::lookAt( glm::vec3( eye[ 0 ], eye[ 1 ], eye[ 2 ] ),
+    //   glm::vec3( eye[ 0 ], eye[ 1 ], eye[ 2 ]-1.0f ),
+    //   glm::vec3( 0.0f, 1.0f, 0.0f ) );
 
+    view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+    float decal = 1.25f;
 
     model = glm::mat4(1.0f);
     model = glm::translate(model, glm::vec3(-decal, -decal, 0.0f));
     displayMesh(maillages[0], model);
 
-
-    model = glm::mat4( 1.0f );
-    model = glm::translate( model, glm::vec3( decal, decal, 0.0f ) );
+    model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(decal, decal, 0.0f));
     displayMesh(maillages[1], model);
 
-    model = glm::mat4( 1.0f );
-    model = glm::translate( model, glm::vec3( -decal, decal, 0.0f ) );
+    model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(-decal, decal, 0.0f));
     displayMesh(maillages[2], model);
 
-    model = glm::mat4( 1.0f );
-    model = glm::translate( model, glm::vec3( decal, -decal, 0.0f ) );
+    model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(decal, -decal, 0.0f));
     displayMesh(maillages[3], model);
 
-
-    
     glutSwapBuffers();
 }
 
 void idle()
 {
-    angle += 0.05f;
-    if( angle >= 360.0f )
+    calcTime();
+
+    angle += 0.025f;
+    if (angle >= 360.0f)
     {
         angle = 0.0f;
     }
 
-//    if( scale <= 0.0f )
-//    {
-//        inc = 0.1f;
-//    }
-//    else if( scale > 2.0f )
-//    {
-//        inc = -0.1f;
-//    }
-//
-//    scale += inc;
+    if (scale <= 0.0f)
+    {
+        inc = 0.1f;
+    }
+    else if (scale > 2.0f)
+    {
+        inc = -0.1f;
+    }
+
+    scale += inc;
 
     glutPostRedisplay();
 }
 
-
-void reshape( int w, int h )
+void reshape(int w, int h)
 {
     glViewport(0, 0, w, h);
     // Modification de la matrice de projection Ã  chaque redimensionnement de la fenÃªtre.
-    proj = glm::perspective( 45.0f, w/static_cast< float >( h ), 0.1f, 100.0f );
+    proj = glm::perspective(45.0f, w / static_cast<float>(h), 0.1f, 100.0f);
 }
 
-
-void special( int key, int x, int y )
+void special(int key, int x, int y)
 {
     switch (key)
     {
-        case GLUT_KEY_LEFT:
-            rotationY -= 5.0f;
-            break;
-        case GLUT_KEY_RIGHT:
-            rotationY += 5.0f;
-            break;
-        case GLUT_KEY_UP:
-            rotationX -= 5.0f;
-            break;
-        case GLUT_KEY_DOWN:
-            rotationX += 5.0f;
-            break;
+    case GLUT_KEY_UP: // Avancer
+        cameraPos += 0.05f * cameraFront;
+        break;
+    case GLUT_KEY_DOWN: // Reculer
+        cameraPos -= 0.05f * cameraFront;
+        break;
+    case GLUT_KEY_LEFT: // gauche
+        cameraPos -= 0.05f * cameraRight;
+        break;
+    case GLUT_KEY_RIGHT: // droite
+        cameraPos += 0.05f * cameraRight;
+        break;
+    case GLUT_KEY_CTRL_L:
+        cameraPos -= 0.05f * cameraUp;
+        break;
     }
+
+    updateCameraMouse();
     glutPostRedisplay();
 }
 
+// La fonction pour gérer les touches normales
+void key(unsigned char key, int x, int y)
+{
+    if (key == 27) // Esc
+    {
+        exit(0); // Ferme l'application
+    }
+}
 
-maillage initVAOs(const shaderProg shader, const std::string& meshPath)
+maillage initVAOs(const shaderProg shader, const std::string &meshPath)
 {
     unsigned int vboids[4];
 
-    std::ifstream ifs(MY_SHADER_PATH+meshPath);
+    std::ifstream ifs(MY_SHADER_PATH + meshPath);
 
     std::string off;
     unsigned int nbpoints, tmp;
@@ -204,11 +302,13 @@ maillage initVAOs(const shaderProg shader, const std::string& meshPath)
     std::vector<unsigned int> indices(nbtriangles * 3);
     std::vector<float> normals(nbpoints * 3);
 
-    for (unsigned int i = 0; i < vertices.size(); ++i) {
+    for (unsigned int i = 0; i < vertices.size(); ++i)
+    {
         ifs >> vertices[i];
     }
 
-    for (unsigned int i = 0; i < nbtriangles; ++i) {
+    for (unsigned int i = 0; i < nbtriangles; ++i)
+    {
         ifs >> tmp;
         ifs >> indices[i * 3];
         ifs >> indices[i * 3 + 1];
@@ -222,13 +322,20 @@ maillage initVAOs(const shaderProg shader, const std::string& meshPath)
     xmin = xmax = vertices[0];
     ymin = ymax = vertices[1];
     zmin = zmax = vertices[2];
-    for (unsigned int i = 1; i < nbpoints; ++i) {
-        if (xmin > vertices[i * 3]) xmin = vertices[i * 3];
-        if (xmax < vertices[i * 3]) xmax = vertices[i * 3];
-        if (ymin > vertices[i * 3 + 1]) ymin = vertices[i * 3 + 1];
-        if (ymax < vertices[i * 3 + 1]) ymax = vertices[i * 3 + 1];
-        if (zmin > vertices[i * 3 + 2]) zmin = vertices[i * 3 + 2];
-        if (zmax < vertices[i * 3 + 2]) zmax = vertices[i * 3 + 2];
+    for (unsigned int i = 1; i < nbpoints; ++i)
+    {
+        if (xmin > vertices[i * 3])
+            xmin = vertices[i * 3];
+        if (xmax < vertices[i * 3])
+            xmax = vertices[i * 3];
+        if (ymin > vertices[i * 3 + 1])
+            ymin = vertices[i * 3 + 1];
+        if (ymax < vertices[i * 3 + 1])
+            ymax = vertices[i * 3 + 1];
+        if (zmin > vertices[i * 3 + 2])
+            zmin = vertices[i * 3 + 2];
+        if (zmax < vertices[i * 3 + 2])
+            zmax = vertices[i * 3 + 2];
     }
 
     // Calcul du centre de la boÃ®te englobante
@@ -297,8 +404,7 @@ maillage initVAOs(const shaderProg shader, const std::string& meshPath)
 
     glBindBuffer(GL_ARRAY_BUFFER, vboids[0]);
     glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
-    
-    
+
     auto pos = glGetAttribLocation(shader.progid, "in_pos");
     glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(pos);
@@ -310,8 +416,7 @@ maillage initVAOs(const shaderProg shader, const std::string& meshPath)
     // VBO contenant les normales.
     glBindBuffer(GL_ARRAY_BUFFER, vboids[3]);
     glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(float), normals.data(), GL_STATIC_DRAW);
-   
-   
+
     auto normal = glGetAttribLocation(shader.progid, "in_normal");
     glVertexAttribPointer(normal, 3, GL_FLOAT, GL_TRUE, 0, 0);
     glEnableVertexAttribArray(normal);
@@ -333,9 +438,7 @@ maillage initVAOs(const shaderProg shader, const std::string& meshPath)
     return m;
 }
 
-
-
-shaderProg initShaders(const std::string& pathVert, const std::string& pathFrag)
+shaderProg initShaders(const std::string &pathVert, const std::string &pathFrag)
 {
     unsigned int vsid, fsid;
     int status;
@@ -394,7 +497,6 @@ shaderProg initShaders(const std::string& pathVert, const std::string& pathFrag)
         std::cerr << log << std::endl;
     }
 
-    
     progid = glCreateProgram();
     glAttachShader(progid, vsid);
     glAttachShader(progid, fsid);
@@ -406,34 +508,35 @@ shaderProg initShaders(const std::string& pathVert, const std::string& pathFrag)
     shader.mid = glGetUniformLocation(progid, "m");
     shader.vid = glGetUniformLocation(progid, "v");
     shader.pid = glGetUniformLocation(progid, "p");
-    
+
     return shader;
 }
 
-
-
-int main( int argc, char * argv[] )
+int main(int argc, char *argv[])
 {
-    glutInit( &argc, argv );
+    glutInit(&argc, argv);
 #if defined(__APPLE__) && defined(ENABLE_SHADERS)
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA|GLUT_3_2_CORE_PROFILE);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA | GLUT_3_2_CORE_PROFILE);
 #else
 
-glutInitContextVersion( 3, 2 );
-//glutInitContextVersion( 4, 5 );
+    glutInitContextVersion(3, 2);
     glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
     glewInit();
 #endif
 
-    glutInitWindowSize( 640, 480 );
+    glutInitWindowSize(1000, 960);
 
+    glutCreateWindow(argv[0]);
 
-    glutCreateWindow( argv[ 0 ]  );
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutIdleFunc(idle);
+    glutSpecialFunc(special);
+    glutKeyboardFunc(key);
 
-    glutDisplayFunc( display );
-    glutReshapeFunc( reshape );
-    glutIdleFunc( idle );
-    glutSpecialFunc( special );
+    // Configurer la gestion des mouvements de la souris
+    glutPassiveMotionFunc(deplacementSouris);
+    glutSetCursor(GLUT_CURSOR_NONE); // Cache le curseur de la souris pour un contrôle libre
 
     // Initialisation de la bibliothÃ¨que GLEW.
 #if not defined(__APPLE__)
@@ -441,18 +544,18 @@ glutInitContextVersion( 3, 2 );
 #endif
 
     glEnable(GL_DEPTH_TEST);
-    shaders[0]=initShaders("/shaders/phong.vert.glsl","/shaders/phong.frag.glsl");
-    maillages[0]=initVAOs(shaders[0],"/meshes/space_shuttle2.off");
-    shaders[1]=initShaders("/shaders/phong.vert.glsl","/shaders/toon.frag.glsl");
-    maillages[1]=initVAOs(shaders[1],"/meshes/space_station2.off");
-    shaders[2]=initShaders("/shaders/phong.vert.glsl","/shaders/phongVert.frag.glsl");
-    maillages[2]=initVAOs(shaders[2],"/meshes/milleniumfalcon.off");
-    shaders[3]=initShaders("/shaders/phong.vert.glsl","/shaders/phongRouge.frag.glsl");
-    maillages[3]=initVAOs(shaders[3],"/meshes/rabbit.off");
+    shaders[0] = initShaders("/shaders/phong.vert.glsl", "/shaders/phong.frag.glsl");
+    maillages[0] = initVAOs(shaders[0], "/meshes/space_shuttle2.off");
+    shaders[1] = initShaders("/shaders/phong.vert.glsl", "/shaders/toon.frag.glsl");
+    maillages[1] = initVAOs(shaders[1], "/meshes/space_station2.off");
+    shaders[2] = initShaders("/shaders/phong.vert.glsl", "/shaders/phongVert.frag.glsl");
+    maillages[2] = initVAOs(shaders[2], "/meshes/milleniumfalcon.off");
+    shaders[3] = initShaders("/shaders/phong.vert.glsl", "/shaders/phongRouge.frag.glsl");
+    maillages[3] = initVAOs(shaders[3], "/meshes/rabbit.off");
     rep.init();
     check_gl_error();
 
-    glClearColor( 0.0f, 0.0f, 0.0f, 0.0f );
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
     glutMainLoop();
 
